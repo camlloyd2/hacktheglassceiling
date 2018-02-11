159f61296bcb919583dfa8904e9ded4c4f9e1e5d
camlloyd2 <cvl8ec@irginia.edu>
2018-02-10 22:19:32 -0500

    add bot commands

--- src/App.js
+++ src/App.js
@@ -9,17 +9,25 @@ import background from './background.png';
 import brokenglass from './brokenglass.png';
 import Linkify from 'react-linkify';
 import pearlGirl from './pearlGirl.jpeg';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton'
+import {ThemeProvider} from 'styled-components';
+import {
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardTitle,
+  CardText,
+} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <div style={{backgroundImage: 'url(' + background + ')'}}>
+        <div style={{backgroundImage: 'url(' + background + ')', opacity: 0.8}}>
           <header className="App-header">
             <h1
               style={{
-                fontSize: 50,
+                fontSize: 60,
                 fontFamily: 'didot',
                 fontStyle: 'bold',
                 textShadow: '3px 3px 3px  #04080f',
@@ -32,67 +40,77 @@ class App extends Component {
 
         <Tabs>
           <Tab label="Home">
-          <div style={{backgroundImage: 'url(' + brokenglass + ')'}}>
-               
-            <div style={{margin: 60}}>
-              <h2>About Hacking the Glass Ceiling</h2>
-              <p />
-              <p>
-                Empowering women through resources
-              </p>
+            <div style={{backgroundImage: 'url(' + brokenglass + ')'}}>
+
+              <div style={{margin: 60}}>
+                <h2>About Hacking the Glass Ceiling</h2>
+                <p />
+                <p>
+                  Empowering women through resources
+                </p>
 
               </div>
             </div>
           </Tab>
           <Tab label="Resources">
             <div style={{margin: 60}}>
-            <h2 style={{fontsize: 32}}>Resources</h2>
-  <Card>
-    <CardHeader
-      title="URL Avatar"
-      subtitle="Subtitle"
-      avatar="images/jsa-128.jpg"
-    />
-    <CardMedia
-      overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-    >
-      <img src="images/nature-600-337.jpg" alt="" />
-    </CardMedia>
-    <CardTitle title="Card title" subtitle="Card subtitle" />
-    <CardText>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-    </CardText>
-    <CardActions>
-      <FlatButton label="Action1" />
-      <FlatButton label="Action2" />
-    </CardActions>
-  </Card>
-  <Card>
-    <CardHeader
-      title="URL Avatar"
-      subtitle="Subtitle"
-      avatar="images/jsa-128.jpg"
-    />
-    <CardMedia
-      overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-    >
-      <img src="images/nature-600-337.jpg" alt="" />
-    </CardMedia>
-    <CardTitle title="Card title" subtitle="Card subtitle" />
-    <CardText>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-    </CardText>
-    <CardActions>
-      <FlatButton label="Action1" />
-      <FlatButton label="Action2" />
-    </CardActions>
-  </Card>
+              <h2 style={{fontsize: 32}}>Resources</h2>
+              <Card>
+                <CardHeader
+                  title="URL Avatar"
+                  subtitle="Subtitle"
+                  avatar="images/jsa-128.jpg"
+                />
+                <CardMedia
+                  overlay={
+                    <CardTitle
+                      title="Overlay title"
+                      subtitle="Overlay subtitle"
+                    />
+                  }
+                >
+                  <img src="images/nature-600-337.jpg" alt="" />
+                </CardMedia>
+                <CardTitle title="Card title" subtitle="Card subtitle" />
+                <CardText>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                  Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                  Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                </CardText>
+                <CardActions>
+                  <FlatButton label="Action1" />
+                  <FlatButton label="Action2" />
+                </CardActions>
+              </Card>
+              <Card>
+                <CardHeader
+                  title="URL Avatar"
+                  subtitle="Subtitle"
+                  avatar="images/jsa-128.jpg"
+                />
+                <CardMedia
+                  overlay={
+                    <CardTitle
+                      title="Overlay title"
+                      subtitle="Overlay subtitle"
+                    />
+                  }
+                >
+                  <img src="images/nature-600-337.jpg" alt="" />
+                </CardMedia>
+                <CardTitle title="Card title" subtitle="Card subtitle" />
+                <CardText>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                  Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                  Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+                </CardText>
+                <CardActions>
+                  <FlatButton label="Action1" />
+                  <FlatButton label="Action2" />
+                </CardActions>
+              </Card>
             </div>
           </Tab>
           <Tab label="Chat">
@@ -137,11 +155,13 @@ class App extends Component {
 
               </div>
               <Paper>
-                <ChatBot
-                  userAvatar={pearlGirl}
-                  recognitionEnable={true}
-                  steps={steps}
-                />
+                <ThemeProvider theme={theme}>
+                  <ChatBot
+                    userAvatar={pearlGirl}
+                    recognitionEnable={true}
+                    steps={steps}
+                  />
+                </ThemeProvider>
               </Paper>
             </div>
 
@@ -151,6 +171,18 @@ class App extends Component {
     );
   }
 }
+
+const theme = {
+  background: '#f5f8fb',
+  fontFamily: 'didot',
+  headerBgColor: '#00BCD4',
+  headerFontColor: '#fff',
+  headerFontSize: '15px',
+  botBubbleColor: '#00BCD4',
+  botFontColor: '#fff',
+  userBubbleColor: '#fff',
+  userFontColor: '#4a4a4a',
+};
 
 const steps = [
   {
@@ -171,6 +203,38 @@ const steps = [
   {
     id: '3',
     message: 'Hi {previousValue}, nice to meet you!',
+    trigger: '4',
+  },
+  {
+    id: '4',
+    message: "Let's run through a scenario, which one would you like to practice?",
+    trigger: '5',
+  },
+  {
+    id: '5',
+    options: [
+      {
+        value: 1,
+        label: 'Negotiate a salary',
+        trigger: '6',
+      },
+      {value: 2, label: 'Discuss a promotion', trigger: '7'},
+      {value: 3, label: 'Confronting workplace discrimination', trigger: '8'},
+    ],
+  },
+  {
+    id: '6',
+    message: 'Employer: "I am sorry, we can only offer---',
+    end: true,
+  },
+  {
+    id: '7',
+    message: 'Employer: "We do not feel that you are ready for that role."',
+    end: true,
+  },
+  {
+    id: '8',
+    message: 'Employer/Colleague: "Are you sure you can handle that? You do not seem capable."',
     end: true,
   },
 ];
